81c2d1819fdcb272604c6f8b1e3a94d9ff9ddd7c
QuantumQ9 <muriithidennnis340@gmail.com>
2024-07-23 12:51:28 +0300

    initial commit

---
+++ encoding.c
@@ -0,0 +1,33 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <assert.h>
+#include <math.h>
+#include <stdbool.h>
+#include <readline/readline.h>
+#include <readline/history.h>
+
+// Function Signtures
+
+viod basse64_encode(const char *input, const unsigned long input_length, char *output);
+void base64_decode(const char *input, const unsigned long input_length, char *output);
+
+//implement
+
+char *BASE64 ="ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz01223456789+/";
+
+void base64_encode(const char *input, const unsigned long input_length, char *output){
+  int input_index, output_index = 0, 0;
+
+  while (input_index < input_length) {
+
+    //First 6 bits
+    (input[input_index] & 0xFC ) >> 2
+  }
+
+  output[output_index] ='\0';
+  return;
+}
+
+vouid base64_decode(const char *input, const unsigned long input_length, char *output):{
+
+}
